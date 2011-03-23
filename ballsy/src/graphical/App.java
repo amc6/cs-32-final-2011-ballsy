3b1db9a678b0ea6ba09cb3af79a12452df191f11
igoldbergvt@gmail.com <igoldbergvt@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-03-23 01:58:23 +0000

    re-added source files

---
+++ ballsy/src/graphical/App.java
@@ -0,0 +1,15 @@
+package graphical;
+
+import processing.core.PApplet;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(new String[] { "--present", "graphical.Level" });
+	}
+
+}
