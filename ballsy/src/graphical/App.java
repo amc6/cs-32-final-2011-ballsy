4fec2299ffac281d2b2ebe4442ce02f6781c4a49
igoldbergvt@gmail.com <igoldbergvt@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-03-23 19:27:03 +0000

    re-adding SRC files...oops.

---
+++ ballsy/src/graphical/App.java
@@ -0,0 +1,18 @@
+package graphical;
+
+import processing.core.PApplet;
+
+// test comment
+
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(new String[] { "--present", "graphical.Level" });
+	}
+
+}
---
+++ ballsy/src/graphical/Surface.java
@@ -0,0 +1,90 @@
+package graphical;
+
+import java.util.ArrayList;
+
+import org.jbox2d.collision.shapes.EdgeChainDef;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+
+import pbox2d.PBox2D;
+import processing.core.PApplet;
+
+//The Nature of Code
+//<http://www.shiffman.net/teaching/nature>
+//Spring 2010
+//PBox2D example
+
+//An uneven surface boundary
+
+class Surface {
+	// We'll keep track of all of the surface points
+	ArrayList<Vec2> surface;
+	private Level _level;
+	private PBox2D _box2d;
+
+
+	Surface(Level level, PBox2D box2d) {
+		_level = level;
+		_box2d = box2d;
+		surface = new ArrayList<Vec2>();
+
+		// This is what box2d uses to put the surface in its world
+		EdgeChainDef edges = new EdgeChainDef();
+
+		// Perlin noise argument
+		float xoff = 0.0f;
+
+		// This has to go backwards so that the objects  bounce off the top of the surface
+		// This "edgechain" will only work in one direction!
+		for (float x = _level.width+10; x > -10; x -= 5) {
+
+			// Doing some stuff with perlin noise to calculate a surface that points down on one side
+			// and up on the other
+			float y;
+			if (x > _level.width/2) {
+				y = 100 + (_level.width - x)*1.1f + PApplet.map(_level.noise(xoff),0,1,-80,80);
+			} 
+			else {
+				y = 100 + x*1.1f + PApplet.map(_level.noise(xoff),0,1,-80,80);
+			}
+
+			// The edge point in our window
+			Vec2 screenEdge = new Vec2(x,y);
+			// We store it for rendering
+			surface.add(screenEdge);
+
+			// Convert it to the box2d world and add it to our EdgeChainDef
+			Vec2 edge = _box2d.coordPixelsToWorld(screenEdge);
+			edges.addVertex(edge);
+
+			// Move through perlin noise
+			xoff += 0.1;
+
+		}
+		edges.setIsLoop(false);   // We could make the edge a full loop
+		edges.friction = 2.0f;    // How much friction
+		edges.restitution = 0.3f; // How bouncy
+
+		// The edge chain is now a body!
+		BodyDef bd = new BodyDef();
+		bd.position.set(0.0f,0.0f);
+		Body body = _box2d.world.createBody(bd);
+		body.createShape(edges);
+
+	}
+
+	// A simple function to just draw the edge chain as a series of vertex points
+	void display() {
+		_level.strokeWeight(2);
+		_level.stroke(0);
+		_level.noFill();
+		_level.beginShape();
+		for (Vec2 v: surface) {
+			_level.vertex(v.x,v.y);
+		}
+		_level.endShape();
+	}
+
+}
+
