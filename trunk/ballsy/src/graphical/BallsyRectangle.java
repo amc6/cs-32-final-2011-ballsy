2f97229f2ee4663f815fb841ebe2216c3098aee7
hazmattron@gmail.com <hazmattron@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-04-05 15:15:02 +0000

    Changes made by Isaac on April 4th.

---
+++ trunk/ballsy/src/graphical/BallsyRectangle.java
@@ -0,0 +1,37 @@
+package graphical;
+
+import pbox2d.PBox2D;
+
+public class BallsyRectangle extends BallsyObject {
+	
+	public BallsyRectangle(float xPos, float yPos, float width, float height, PBox2D box2d, Level level){
+		super(box2d, level);
+		
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void killBody() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void makeBody() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
