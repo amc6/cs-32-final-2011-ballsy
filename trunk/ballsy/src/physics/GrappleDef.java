c168288c832a232dbaacd91e2def15f7afc27f96
jeliumon@gmail.com <jeliumon@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-04-21 03:39:57 +0000

    Added more accessors to AbstractBody and PhysicsDef Created Grapple/GrappleDef classes TestGrappleLevel displays grappling line on click, no joints yet

---
+++ trunk/ballsy/src/physics/GrappleDef.java
@@ -0,0 +1,28 @@
+package physics;
+
+public class GrappleDef extends PhysicsDef {
+
+	public GrappleDef(PhysicsWorld world, boolean mobile) {
+		super(world, mobile);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public float getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRadius() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
