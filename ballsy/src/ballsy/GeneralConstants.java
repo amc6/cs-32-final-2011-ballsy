6923e33380852a4a0ff645c7116b28e44413fe09
jeliumon@gmail.com <jeliumon@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-02 16:43:24 +0000

    Fixed some texty things.

--- ballsy/src/ballsy/GeneralConstants.java
+++ ballsy/src/ballsy/GeneralConstants.java
@@ -5,10 +5,12 @@ import processing.core.*;
 public final class GeneralConstants {
 
 	public static final int DEFAULT_LINE_WIDTH = 0;
-	
+	public static final PFont DEFAULT_FONT_SMALLER = Window.getInstance().loadFont("StrandedBRK-30.vlw");
 	public static final PFont DEFAULT_FONT = Window.getInstance().loadFont("StrandedBRK-38.vlw");
 	public static final PFont DEFAULT_FONT_BIGGER = Window.getInstance().loadFont("StrandedBRK-40.vlw");
 	public static final PFont DEFAULT_FONT_TITLE = Window.getInstance().loadFont("StrandedBRK-72.vlw");
+	public static final PFont STUPID_FONT = Window.getInstance().createFont("Verdana", 20, true);
+
 	public static final int DEFAULT_FONT_INACTIVE = 200;
 	public static final int DEFAULT_FONT_ACTIVE = 255;
 	
