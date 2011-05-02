e28dd56ffb35886abd71d7531a9ab153ba2af72e
jeliumon@gmail.com <jeliumon@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-02 04:15:02 +0000

    Added TextButton and more font sizes kind of.

--- ballsy/src/ballsy/GeneralConstants.java
+++ ballsy/src/ballsy/GeneralConstants.java
@@ -6,6 +6,15 @@ public final class GeneralConstants {
 
 	public static final int DEFAULT_LINE_WIDTH = 0;
 	
+	public static final PFont DEFAULT_FONT = Window.getInstance().loadFont("StrandedBRK-38.vlw");
+	public static final PFont DEFAULT_FONT_BIGGER = Window.getInstance().loadFont("StrandedBRK-40.vlw");
+	public static final PFont DEFAULT_FONT_TITLE = Window.getInstance().loadFont("StrandedBRK-72.vlw");
+	public static final int DEFAULT_FONT_INACTIVE = 200;
+	public static final int DEFAULT_FONT_ACTIVE = 255;
+	
+	
+	public static final float DEFAULT_FONT_HEIGHT = 72f;
+	
 	/* RESOURCES FINAL NAME CONSTANTS */
 	public static final String SMOKE_GRAPHIC = "res/smoke.png";
 	
--- ballsy/src/graphics/Text.java
+++ ballsy/src/graphics/Text.java
@@ -2,14 +2,15 @@ package graphics;
 
 import ballsy.Window;
 import processing.core.PFont;
+import static ballsy.GeneralConstants.*;
 
 public class Text {
 
 	private Window _window;
-	private PFont _font;
-	private int _textAlign = _window.CORNER;
+	private PFont _font = DEFAULT_FONT;
+	protected int _textAlign = _window.CENTER;
 	private int _x, _y;
-	private int _color = 100;
+	private int _color = DEFAULT_FONT_INACTIVE;
 	private String _text;
 
 	public Text(String text, int x, int y) {
@@ -19,9 +20,41 @@ public class Text {
 		_y = y;
 		// The font must be located in the sketch's 
 		// "data" directory to load successfully
-		_font = _window.loadFont("StrandedBRK-48.vlw"); 
-
 	}
+	
+	public void setSize(int size) {
+		switch (size) {
+		case 38:
+			_font = DEFAULT_FONT;
+			break;
+		case 40:
+			_font = DEFAULT_FONT_BIGGER;
+			break;
+		case 72:
+			_font = DEFAULT_FONT_TITLE;
+			break;
+		default:
+			_font = DEFAULT_FONT;
+		}
+	}
+	
+	public Text(String text, int x, int y, int color) {
+		_window = Window.getInstance();
+		_text = text;
+		_x = x;
+		_y = y;
+		_color = color;
+	}
+	
+	public Text(String text, int x, int y, int color, int align) {
+		_window = Window.getInstance();
+		_text = text;
+		_x = x;
+		_y = y;
+		_color = color;
+		_textAlign = align;
+	}
+	
 	
 	public void setAlign(int align) {
 		_textAlign = align;
