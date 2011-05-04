2ca81939fc9e838fb9959b35edb2e8500ee6a419
hazmattron@gmail.com <hazmattron@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-04 02:01:02 +0000

    Also forgot this critical file...

---
+++ ballsy/src/editor/BodyFactory.java
@@ -0,0 +1,124 @@
+package editor;
+
+import static bodies.BodyConstants.DEFAULT_BALL_RADIUS;
+import static bodies.BodyConstants.DEFAULT_BODY_BOUNCINESS;
+import static bodies.BodyConstants.DEFAULT_BODY_COLOR;
+import static bodies.BodyConstants.DEFAULT_BODY_DENSITY;
+import static bodies.BodyConstants.DEFAULT_BODY_FRICTION;
+import static bodies.BodyConstants.DEFAULT_RECTANGLE_HEIGHT;
+import static bodies.BodyConstants.DEFAULT_RECTANGLE_WIDTH;
+
+import java.util.ArrayList;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+
+import bodies.AbstractBody;
+import bodies.Ball;
+import bodies.IrregularPolygon;
+import bodies.Rectangle;
+import bodies.RegularPolygon;
+
+public class BodyFactory {
+	public static final int RECT = 0, IPOLY = 1, RPOLY = 2, BALL = 3;
+	private int _currBody = RECT;
+	// the  instance variables for current object properties, with default values
+	public float width = DEFAULT_RECTANGLE_WIDTH;
+	public float height = DEFAULT_RECTANGLE_HEIGHT;
+	public float radius = DEFAULT_BALL_RADIUS;
+	public float rotation = 0f;
+	public int color = DEFAULT_BODY_COLOR;
+	public float angularVelocity = 0f;
+	public Vec2 velocity = new Vec2(0, 0);
+	public float density = DEFAULT_BODY_DENSITY;
+	public float friction = DEFAULT_BODY_FRICTION;
+	public float bounciness = DEFAULT_BODY_BOUNCINESS;
+	public boolean deadly = false;
+	public boolean grappleable = true;
+	public boolean fixed = false;
+	public boolean graphicalOnly = false;
+	public int polyPointCount = 3;
+	public ArrayList<Vec2> polyPoints = null;
+	
+	public BodyFactory() { }
+	
+	/**
+	 * Sets the currently selected body in the level editor.
+	 * @param i
+	 */
+	public void setBody(int i) {
+		_currBody = i;
+	}
+	
+	/**
+	 * gets a body of the currently 
+	 * @param pos
+	 * @return
+	 */
+	public AbstractBody getBody(Vec2 pos) {
+		AbstractBody newBody = null;
+		switch (_currBody) {
+		case RECT:
+			// return a rectangle
+			newBody = this.getRect(pos);
+			break;
+		case IPOLY:
+			// return an irregular polygon
+			newBody = this.getIPoly(pos);
+			break;
+		case RPOLY:
+			// return a regular polygon
+			newBody = this.getRPoly(pos);
+			break;
+		case BALL:
+			// return a ball
+			newBody = this.getBall(pos);
+			break;
+		}
+		// set the properties
+		newBody.getPhysicsDef().setMobile(!fixed);
+		newBody.getGraphicsDef().setColor(color);
+		newBody.setGrappleable(grappleable);
+		newBody.setDeadly(deadly);
+		Body body = newBody.getPhysicsDef().getBody();
+		body.setLinearVelocity(velocity);
+		body.setAngularVelocity(angularVelocity);
+		body.m_sweep.a = rotation;
+		body.getShapeList().setFriction(friction);
+		body.getShapeList().setRestitution(bounciness);
+		body.getShapeList().m_density = density;
+		return newBody;
+	}
+	
+	/**
+	 * Return a ball with currently set properties.
+	 * @return
+	 */
+	private Ball getBall(Vec2 pos) {
+		return new Ball(pos.x, pos.y, radius);
+	}
+	
+	/**
+	 * Return a rectangle with currently set properties.
+	 * @return
+	 */
+	private Rectangle getRect(Vec2 pos) {
+		return new Rectangle(pos.x, pos.y, width, height);
+	}
+	
+	/**
+	 * Return an irregular polygon with currently set properties.
+	 * @return
+	 */
+	private IrregularPolygon getIPoly(Vec2 pos) {
+		return new IrregularPolygon(pos.x, pos.y, polyPoints);
+	}
+	
+	/**
+	 * Return an regular polygon with currently set properties.
+	 * @return
+	 */
+	private RegularPolygon getRPoly(Vec2 pos) {
+		return new RegularPolygon(pos.x, pos.y, polyPointCount, radius);
+	}
+}
