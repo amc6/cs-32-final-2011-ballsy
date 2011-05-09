f37fb2341ba4eee577be28c807f29f91050c1020
igoldbergvt@gmail.com <igoldbergvt@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-09 10:01:31 +0000

    Fixed save and load buttons finally and also fixed a few focus bugs. Also made a GenericLevel.xml that all future levels should use as a base!

--- ballsy/src/editor/SaveButton.java
+++ ballsy/src/editor/SaveButton.java
@@ -1,21 +1,22 @@
 package editor;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.RunnableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
-import ballsy.XMLUtil;
-
 import processing.core.PConstants;
 import processing.core.PImage;
+import ballsy.AbstractLevel;
+import ballsy.XMLUtil;
 
 public class SaveButton extends AbstractButton {
 
 	private PImage _image;
-	private String _name;
-	private boolean _save = false;
 	
 	public SaveButton(LevelEditor editor, BodyFactory factory, int minX, int minY, int maxX, int maxY) {
 
@@ -32,67 +33,37 @@ public class SaveButton extends AbstractButton {
 		_window.popMatrix();
 	}
 
-
-
 	public void select() {
-		System.out.println("SAVE!");
-		
-//		_fileChooser.showSaveDialog(null);
-
-		Runnable saveTask = new Runnable() {
-//			public String fileName;
-//			public boolean done = false;
-			
+		_clicked = false; 
+	
+		SwingUtilities.invokeLater(new Runnable(){
 			public void run() {
 				try {
 					JFileChooser fc = new JFileChooser();
-				    // Set the current directory
-				    File f = new File(new File("./levels").getCanonicalPath());
-				    fc.setCurrentDirectory(f);
-				    
+					// Set the current directory
+					File f = new File(new File("./levels").getCanonicalPath());
+					fc.setCurrentDirectory(f);
+
 					int returnVal = fc.showSaveDialog(null);
 					if (returnVal == JFileChooser.APPROVE_OPTION) {
 						File file = fc.getSelectedFile();
-//						boolean executeOperation = false;
-//						if(file.getName().endsWith("jpg") || file.getName().endsWith("gif")){
-//							//temp = loadImage(file.getPath());
-//
-//						}
+
 						String filename = file.getName();
 						if(filename.endsWith("xml")) {
-//							System.out.println(file.getName());
-							_name = filename.substring(0, filename.indexOf('.'));
-							_save = true;
+
+							_level.setSave(filename.substring(0, filename.indexOf('.')));
+
+							
 						}
-						
+
 					}
 				}
 				catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		};
-		try { SwingUtilities.invokeAndWait(saveTask);} 
-		catch (InterruptedException e) { e.printStackTrace(); } 
-		catch (InvocationTargetException e) { e.printStackTrace();}
-		
-		_clicked = false; // make button inactive
-		if (_save) { // because we need this in the OG thread yo.
-			save(_name);
-			_save = false;
-		}
-	}
-	
-	/**
-	 * 
-	 * @param name Name of level
-	 */
-	public void save(String name) {
-		String pathname = "levels/" + name + ".xml";
-		String thumbPathname = "levels/thumbs/" + name + ".png";
-		XMLUtil.getInstance().writeFile(_level, pathname);
-		_level.makeThumbnail(thumbPathname);
-		XMLUtil.getInstance().addMenuButton(pathname, thumbPathname);
+		});		
+		 
 	}
 	
 
