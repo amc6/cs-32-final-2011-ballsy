75a278770ba2a380f1ab16e4d35c8f6d4ba59efd
jeliumon@gmail.com <jeliumon@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-09 08:53:29 +0000

    Clouds in Menus and shit

---
+++ trunk/ballsy/src/graphics/Cloud.java
@@ -0,0 +1,61 @@
+package graphics;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import ballsy.Window;
+
+public class Cloud {
+	
+	private Vector<CloudPiece> _cloud;
+	private int _radiusBig,_radiusMed,_radiusSmall;
+	private int _counter, _maxCount,_x,_y;
+	private Window _window;
+	
+	public Cloud(int centerX, int centerY, int bigRadius) {
+		_window = Window.getInstance();
+		_cloud = new Vector<CloudPiece>();
+		_x = centerX;
+		_y = centerY;
+		
+		_radiusBig = bigRadius;
+		_radiusMed = (int) (bigRadius*.625);
+		_radiusSmall = bigRadius/4;
+		
+		_counter = 0;
+		_maxCount = (int) Math.ceil(40/bigRadius);
+		
+		_cloud.add(new CloudPiece(centerX, centerY, _radiusBig));
+		_cloud.add(new CloudPiece(centerX-_radiusBig, centerY, _radiusBig));
+		_cloud.add(new CloudPiece(centerX+_radiusBig, centerY, _radiusBig));
+		_cloud.add(new CloudPiece(centerX, centerY-_radiusBig, _radiusBig));
+		_cloud.add(new CloudPiece(centerX-_radiusBig-_radiusBig, centerY+_radiusMed/2, _radiusMed));
+		_cloud.add(new CloudPiece(centerX+_radiusBig+_radiusBig, centerY+_radiusMed/2, _radiusMed));
+	}
+	
+	public void draw() {
+		for (Iterator<CloudPiece> i = _cloud.iterator(); i.hasNext();) {
+			i.next().draw();
+		}
+	}
+
+	public void drift(int dx) {
+		if (_x > _window.width*2) { //wrap
+			for (Iterator<CloudPiece> i = _cloud.iterator(); i.hasNext();) {
+				i.next().setBackX(-_window.width*2-500);
+			}
+			_x -= (_window.width*2+500);
+		}
+		//move every other whatever
+		if (_counter == _maxCount) {
+			_counter = 0;
+			for (Iterator<CloudPiece> i = _cloud.iterator(); i.hasNext();) {
+				i.next().drift(dx);
+			}
+			_x += dx;
+		}
+		else {
+			_counter++;
+		}
+	}
+}
