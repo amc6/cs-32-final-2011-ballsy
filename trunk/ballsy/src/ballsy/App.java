b93082e3124116199fc12ea8030a0c9fdcee0f69
hazmattron@gmail.com <hazmattron@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-13 01:27:18 +0000

    Now with update checking stuff! Ballsy now reads version_info.txt from the server and checks its current version number (stored in GeneralConstants) against the first line of this file, and then notifies the user with the next lines of the file, whatever updates are included in the new version.

--- trunk/ballsy/src/ballsy/App.java
+++ trunk/ballsy/src/ballsy/App.java
@@ -1,17 +1,83 @@
 package ballsy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.swing.JOptionPane;
+
 import processing.core.PApplet;
 
 public class App {
 
 	/**
 	 * @param args
+	 * @throws URISyntaxException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PApplet.main(new String[] { "--present", "ballsy.Window" });
-		System.out.println("Welcome to Ballsy!");
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		int runCode = 0; // by default, just run Ballsy
+		try {
+			// check this copy's version number against the online version, if online.
+			URL versionFile = new URL("http://www.beballsy.com/version_info.txt");
+			URLConnection conn = versionFile.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			boolean upToDate = false;
+			if ((inputLine = in.readLine()) != null && inputLine.equals(GeneralConstants.VERSION_NUMBER)) upToDate = true;
+			if (!upToDate) {
+				String message = "A newer version of Ballsy is available. For best performance, " +
+			    "please update to the most recent release.\n\n" +
+			    "Revision " + inputLine + ":";
+				while ((inputLine = in.readLine()) != null && inputLine != "") {
+					message = message + "\n" + "-> " + inputLine;
+				}
+				
+				Object[] options = {"Play Outdated Version", "Get Update"};
+				runCode = JOptionPane.showOptionDialog(null,
+				    wrapKinda(message, 60),
+				    "Ballsy Update",
+				    JOptionPane.YES_NO_OPTION,
+				    JOptionPane.WARNING_MESSAGE,
+				    null,
+				    options,
+				    options[1]); // now: 1 = get update, 0 = play outdated, -1 = message box closed
+			}
+			// close connection
+			in.close();
+		} catch (Exception e) {
+			// maybe we're offline. Do nothing.
+			System.out.println("No version information available.");
+			e.printStackTrace();
+		}
+		// doo dee right ting
+		if (runCode == 1) {
+			// open the website
+			java.awt.Desktop.getDesktop().browse(new URI("http://www.beballsy.com"));
+		} else {
+			// run ballsy
+			PApplet.main(new String[] { "--present", "ballsy.Window" });
+		}
 	}
 
+	private static String wrapKinda(String str, int size) {
+		String[] lines = str.split("\n");
+		String output = "";
+		for (String s : lines) {
+			if (output.length() > 0) output = output + "\n";
+			if (s.length() > size) {
+				int position = size;
+				while (!s.substring(position, position+1).equals(" ") && position+1 < s.length()) position ++;
+				if (position != s.length() - 1) position ++;
+				output = output + s.substring(0, position) + "\n" + wrapKinda(s.substring(position, s.length()), size);
+			} else output = output + s;
+		}
+		return output;
+	}
+	
 }
 
--- trunk/ballsy/src/ballsy/GeneralConstants.java
+++ trunk/ballsy/src/ballsy/GeneralConstants.java
@@ -3,7 +3,9 @@ package ballsy;
 import processing.core.*;
 
 public final class GeneralConstants {
-
+	/* THE VERSION NUMBER - change this before each release! */
+	public static final String VERSION_NUMBER = "1.0.0";
+	
 	public static final int DEFAULT_LINE_WIDTH = 0;
 	public static final PFont DEFAULT_FONT_SMALLER = Window.getInstance().loadFont("StrandedBRK-30.vlw");
 	public static final PFont DEFAULT_FONT = Window.getInstance().loadFont("StrandedBRK-38.vlw");
