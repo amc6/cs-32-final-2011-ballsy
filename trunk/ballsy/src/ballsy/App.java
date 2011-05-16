7730ce7d2de393f6f0162176f944b4d672daedfb
hazmattron@gmail.com <hazmattron@gmail.com@44dba343-7acc-9463-4a6e-1f653ce1763d>
2011-05-16 21:30:08 +0000

    moved config.txt file to res, and moved read to constructor in Background (so it doesn't have to read it every draw).

--- trunk/ballsy/src/ballsy/App.java
+++ trunk/ballsy/src/ballsy/App.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
